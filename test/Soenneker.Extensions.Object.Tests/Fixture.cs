bd47fb671471a9f48c29a02f494a8cbd5ddda2c8
Jake Soenneker <jake@soeneker.com>
2025-09-09 16:05:45 -0500

    Fixes logger disposal

--- test/Soenneker.Extensions.Object.Tests/Fixture.cs
+++ test/Soenneker.Extensions.Object.Tests/Fixture.cs
@@ -17,7 +17,7 @@ public class Fixture : UnitFixture
 
     private static void SetupIoC(IServiceCollection services)
     {
-        services.AddLogging(builder => { builder.AddSerilog(dispose: true); });
+        services.AddLogging(builder => { builder.AddSerilog(dispose: false); });
 
         IConfiguration config = TestUtil.BuildConfig();
         services.AddSingleton(config);
